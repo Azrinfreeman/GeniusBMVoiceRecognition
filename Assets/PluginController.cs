2eb04341d087bc496682cf180fb8ba202291a1f5
Azrinfreeman <azrinfreeman2@gmail.com>
2025-03-26 19:48:05 +0800

    Initial commit

---
+++ Assets/PluginController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PluginController : MonoBehaviour
+{
+    public Transform[] voicePlugins;
+
+    public Transform[] micButtons;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (CheckInternet.instance.isWifiConnected || CheckInternet.instance.isMobileConnected)
+        {
+
+            if (togglePlugin.instance.isToggle)
+            {
+                Debug.Log("internet ");
+                voicePlugins[0].gameObject.SetActive(true);
+                voicePlugins[1].gameObject.SetActive(false);
+                micButtons[0].gameObject.SetActive(true);
+                micButtons[1].gameObject.SetActive(false);
+
+            }
+            else
+            {
+                Debug.Log("not accurate ");
+                voicePlugins[0].gameObject.SetActive(false);
+                voicePlugins[1].gameObject.SetActive(true);
+                micButtons[0].gameObject.SetActive(false);
+                micButtons[1].gameObject.SetActive(true);
+
+            }
+
+        }
+        else
+        {
+            Debug.Log("no internet, not accureate");
+            voicePlugins[0].gameObject.SetActive(false);
+            voicePlugins[1].gameObject.SetActive(true);
+            micButtons[0].gameObject.SetActive(false);
+            micButtons[1].gameObject.SetActive(true);
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
